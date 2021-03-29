46c99ebdb1a77dc0ad44660184bdd07d09df385c
RubensZaes <rubenszaes@gmail.com>
2021-03-29 12:43:44 -0300

    Updating Accessibility

---
+++ assets/scripts/accessibility.js
@@ -0,0 +1,6 @@
+(function(d){
+    var s = d.createElement("script");
+    s.setAttribute("data-account", "r2QZ6PVCch");
+    s.setAttribute("src", "https://accessibilityserver.org/widget.js");
+    (d.body || d.head).appendChild(s);
+})(document)
